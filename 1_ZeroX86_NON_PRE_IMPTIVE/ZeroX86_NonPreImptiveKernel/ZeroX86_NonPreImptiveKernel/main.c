b62be2ecb41ab037e044d4512d2901096ea271d0
MahmoudSaad.ZeroX86 <msammamsa10@gmail.com>
2019-11-24 23:52:18 +0200

    The NonPreImptive Kernel InitCommit. +RTOS_NOTES.txt

---
+++ 1_ZeroX86_NON_PRE_IMPTIVE/ZeroX86_NonPreImptiveKernel/ZeroX86_NonPreImptiveKernel/main.c
@@ -0,0 +1,18 @@
+/*
+ * ZeroX86_NonPreImptiveKernel.c
+ *
+ * Created: 11/24/2019 11:44:34 PM
+ * Author : Mahmo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
